58e56473c8e4433971989da647892b311bc691a5
Lorenzo Toso <LorToso@gmail.com>
2014-10-30 15:38:12 +0100

    Added working example App. Also using Native Code

---
+++ jni/CLEENR_jni.cpp
@@ -0,0 +1,22 @@
+#include <CLEENR_jni.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/contrib/detection_based_tracker.hpp>
+
+#include <string>
+#include <vector>
+
+#include <android/log.h>
+
+#define LOG_TAG "FaceDetection/DetectionBasedTracker"
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
+
+using namespace std;
+//using namespace cv;
+
+JNIEXPORT void JNICALL Java_com_cleenr_cleenr_MainActivity_nativeDetect(JNIEnv * jenv, jclass, jlong thiz, jlong imageGray, jlong faces)
+{
+    LOGD("Java_com_cleenr_cleenr_MainActivity_nativeDetect enter");
+    LOGD("Java_com_cleenr_cleenr_MainActivity_nativeDetect exit");
+}
+
+
---
+++ src/com/cleenr/cleenr/MainActivity.java
@@ -0,0 +1,98 @@
+package com.cleenr.cleenr;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.Mat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import com.cleenr.cleenr.R;
+
+public class MainActivity extends Activity implements CvCameraViewListener2{
+	private static final String TAG = "MainActivity";
+	private CameraBridgeViewBase   mOpenCvCameraView;
+
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    // Load native library after(!) OpenCV initialization
+                    System.loadLibrary("CLEEN_R");
+                    Log.i(TAG, "Native library loaded successfully");
+
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
+		setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.main_activity_surface_view);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+	}
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+    }
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_activity, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId()  == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	
+	
+
+	@Override
+	public void onCameraViewStarted(int width, int height) {	}
+	@Override
+	public void onCameraViewStopped() {	}
+	@Override
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		nativeDetect(0, 0, 0);
+		return inputFrame.rgba();
+	}
+	
+    private static native void nativeDetect(long thiz, long inputImage, long faces);
+}
