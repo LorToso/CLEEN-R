ac59390ff0877de5fc664fe4f545b8c40a2e9096
Sebastian Hüther <sebastian.huether@gmx.de>
2015-02-19 16:26:37 +0100

    Fixed RobotAction

--- AndroidStudioProject/app/src/main/java/com/cleenr/cleen_r/robotcontrolunits/RobotAction.java
+++ AndroidStudioProject/app/src/main/java/com/cleenr/cleen_r/robotcontrolunits/RobotAction.java
@@ -1,11 +1,8 @@
 package com.cleenr.cleen_r.robotcontrolunits;
 
-<<<<<<< 7cee6c46b7080d0745140c389076aa986894943f
 /**
  * Created by hudini on 19.02.2015.
  */
-public class RobotAction {
-=======
 
 public enum RobotAction {
     DRIVE_FORWARD,
@@ -15,5 +12,4 @@ public enum RobotAction {
     TURN_RIGHT_SLOWLY,
     CLOSE_CLAW,
     OPEN_CLAW
->>>>>>> 25863ef34bada5e7c950ab04c1fb55a0631f4fc1
 }
