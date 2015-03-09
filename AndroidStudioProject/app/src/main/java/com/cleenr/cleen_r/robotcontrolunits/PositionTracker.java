05504902622922a1159e663d57c65386719f9738
Sebastian Hüther <sebastian.huether@gmx.de>
2015-03-09 19:15:52 +0100

    Began a basic position tracking by watching the movement commands

--- AndroidStudioProject/app/src/main/java/com/cleenr/cleen_r/robotcontrolunits/PositionTracker.java
+++ AndroidStudioProject/app/src/main/java/com/cleenr/cleen_r/robotcontrolunits/PositionTracker.java
@@ -2,4 +2,64 @@ package com.cleenr.cleen_r.robotcontrolunits;
 
 public class PositionTracker
 {
+    public enum MOVEMENT_DIRECTION
+    {
+        FORWARD, BACKWARD, LEFT, RIGHT
+    }
+
+    private static final double MAX_NXT_METERS_PER_SECOND = 0.5;
+
+    private double posX, posY;
+    private double angle;
+
+    public PositionTracker()
+    {
+        posX = 0.0;
+        posY = 0.0;
+        angle = 0.0;
+    }
+
+    public void addMovement(MOVEMENT_DIRECTION direction, long duration)
+    {
+        switch (direction)
+        {
+            case FORWARD:
+                break;
+            case BACKWARD:
+                break;
+            case LEFT:
+                break;
+            case RIGHT:
+                break;
+        }
+    }
+
+    public void addVector(double x, double y, double rotation)
+    {
+        posX += x;
+        posY += y;
+        angle += rotation;
+    }
+
+    public void resetPosition()
+    {
+        posX = 0.0;
+        posY = 0.0;
+        angle = 0.0;
+    }
+
+    public double getX()
+    {
+        return posX;
+    }
+
+    public double getY()
+    {
+        return posY;
+    }
+
+    public double getAngle()
+    {
+        return angle;
+    }
 }
