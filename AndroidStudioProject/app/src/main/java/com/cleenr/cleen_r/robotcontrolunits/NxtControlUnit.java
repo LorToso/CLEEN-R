110caf4b941a9f99183a456126dfb3e0e8154ee8
Sebastian Hüther <sebastian.huether@gmx.de>
2015-04-15 22:41:14 +0200

    fixed wrong facing of the starting point, but there seems to be problem with driving backwards (the robot searches the starting point in the opposite direction...)

--- AndroidStudioProject/app/src/main/java/com/cleenr/cleen_r/robotcontrolunits/NxtControlUnit.java
+++ AndroidStudioProject/app/src/main/java/com/cleenr/cleen_r/robotcontrolunits/NxtControlUnit.java
@@ -11,6 +11,7 @@ import org.opencv.core.Point;
 
 public class NxtControlUnit implements RobotControlUnit
 {
+    private final String TAG = "NxtControlUnit";
 
     private final NxtTalker       mNxtTalker;
     private final PositionTracker mPosTracker;
@@ -102,8 +103,10 @@ public class NxtControlUnit implements RobotControlUnit
         lastAction = RobotAction.DRIVE_FORWARD;
     }
 
-    public void driveBackward() {
-        try {
+    public void driveBackward()
+    {
+        try
+        {
             Log.d("ControlUnit", "Driving backward");
             mNxtTalker.setMotorSpeed(LEFT_WHEEL_MOTOR, (byte) (-1 * MOTOR_SPEED));
             mNxtTalker.setMotorSpeed(RIGHT_WHEEL_MOTOR, (byte) (-1 * MOTOR_SPEED));
@@ -112,14 +115,17 @@ public class NxtControlUnit implements RobotControlUnit
             mNxtTalker.setMotorSpeed(NxtTalker.MOTOR_PORT_ALL, (byte) 0, NxtTalker.MOTOR_REG_MODE_NONE);
 
             mPosTracker.addMovement(PositionTracker.MOVEMENT_DIRECTION.BACKWARD, MOTOR_SPEED, SLEEP_TIME_TURNING);
-        } catch (InterruptedException e) {
+        }
+        catch (InterruptedException e)
+        {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         lastAction = RobotAction.DRIVE_FORWARD;
     }
 
-    public boolean hasObjectInClaw() {
+    public boolean hasObjectInClaw()
+    {
         // TODO Auto-generated method stub
         return false;
     }
@@ -267,16 +273,21 @@ public class NxtControlUnit implements RobotControlUnit
         double targetAngle = Utils.normalizeAngle(angleStartingPointToRobot + Math.PI);
         double radiansToTurn = Utils.normalizeAngle(targetAngle - robotAngle);
 
+        Log.d(TAG, String.format("target angle: %.3f\u00b0, to turn: %.3f\u00b0",
+                                 Math.toDegrees(targetAngle), Math.toDegrees(radiansToTurn)));
+
         if (radiansToTurn > Math.PI)
         {
             // turning left is the shorter way
-            while (mPosTracker.getAngle() > STARTING_POINT_ANGLE_TOLERANCE / 2.0)
+            while (Utils.normalizeAngle(targetAngle - mPosTracker.getAngle()) >
+                    STARTING_POINT_ANGLE_TOLERANCE / 2.0)
                 turnLeft();
         }
         else
         {
             // turning right is the shorter way
-            while (mPosTracker.getAngle() < (2.0 * Math.PI) - (STARTING_POINT_ANGLE_TOLERANCE / 2.0))
+            while (Utils.normalizeAngle(targetAngle - mPosTracker.getAngle()) <
+                    (2.0 * Math.PI) - (STARTING_POINT_ANGLE_TOLERANCE / 2.0))
                 turnRight();
         }
 
